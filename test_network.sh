2096210eb786f20657a98483ce8493fba7847a88
aqc1 <aqc1@pct.edu>
2022-05-25 10:31:52 -0400

    Added verbosity option, non-verbose by default

--- test_network.sh
+++ test_network.sh
@@ -13,12 +13,34 @@ print_error() {
     echo -e "${RED}\n[-] ${1}${RESET}"
 }
 
+# Usage func
+usage() {
+    echo -e "\n[-] Incorrect usage!"
+    echo -e "Usage: $0 [-v]\n"
+    exit 1
+}
+
 # Grab IP that's not loopback or docker
 ip=$(ip -br a | grep -v "docker" | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v "127.0.0.1")
 
+# Set verbosity
+verbose=0
+while getopts v opt; do
+    case $opt in
+        v) verbose=1 ;;
+        *) usage ;;
+    esac
+done
+
 # Ping yourself
 print_info "Pinging IP of self..."
-ping -c 1 $ip
+if (( $verbose == 0 ))
+then
+    ping -c 1 $ip 2>&1 >/dev/null
+else
+    ping -c 1 $ip
+fi
+
 if [ $? -ne 0 ]
 then
     print_error "Issue with NIC."
@@ -30,7 +52,13 @@ print_info "Pinging default gateway..."
 set -f
 octets=(${ip//./ })
 gateway="${octets[0]}.${octets[1]}.${octets[2]}.1"
-ping -c 1 $gateway
+if (( $verbose == 0 ))
+then
+    ping -c 1 $gateway 2>&1 >/dev/null
+else
+    ping -c 1 $gateway
+fi
+
 if [ $? -ne 0 ]
 then
     print_error "Issue with router."
@@ -39,7 +67,13 @@ fi
 
 # Ping Google DNS server
 print_info "Pinging Google DNS server..."
-ping -c 1 8.8.8.8
+if (( $verbose == 0 ))
+then
+    ping -c 1 8.8.8.8 2>&1 >/dev/null
+else
+    ping -c 1 8.8.8.8
+fi
+
 if [ $? -ne 0 ]
 then
     print_error "Issue with ISP."
@@ -48,7 +82,13 @@ fi
 
 # Ping google.com
 print_info "Pinging Google's domain name..."
-ping -c 1 "google.com"
+if (( $verbose == 0 ))
+then
+    ping -c 1 "google.com" 2>&1 >/dev/null
+else
+    ping -c 1 "google.com"
+fi
+
 if [ $? -ne 0 ]
 then
     print_error "Issue with DNS."
