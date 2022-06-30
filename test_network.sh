3412f4b1f1731d8dd1aabb9cefca2f180f4b05ba
aqc1 <aqc1@pct.edu>
2022-06-29 21:15:06 -0400

    Edited shell script to reflect suggestions from shellcheck.

--- test_network.sh
+++ test_network.sh
@@ -5,15 +5,37 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 RESET=$(tput sgr0)
 
+# Green for Information
 print_info() {
     echo -e "${GREEN}\n[+] ${1}${RESET}"
 }
 
+# Red for Errors
 print_error() {
     echo -e "${RED}\n[-] ${1}${RESET}"
 }
 
-# Usage func
+# Less verbose output
+quiet_ping() {
+    target="$1"
+    err_msg="$2"
+    if ! ping -c 1 "$target" >/dev/null 2>&1; then
+        print_err "$err_msg"
+        exit
+    fi
+}
+
+# More verbose output
+verbose_ping() {
+    target="$1"
+    err_msg="$2"
+    if ! ping -c 1 "$target"; then
+        print_error "$err_msg"
+        exit
+    fi
+}
+
+# Usage upon errors
 usage() {
     echo -e "\n[-] Incorrect usage!"
     echo -e "Usage: $0 [-v]\n"
@@ -34,65 +56,41 @@ done
 
 # Ping yourself
 print_info "Pinging IP of self..."
-if (( $verbose == 0 ))
+if (( verbose == 0 ))
 then
-    ping -c 1 $ip 2>&1 >/dev/null
+    quiet_ping "$ip" "Issue with NIC."
 else
-    ping -c 1 $ip
-fi
-
-if [ $? -ne 0 ]
-then
-    print_error "Issue with NIC."
-    exit
+    verbose_ping "$ip" "Issue with NIC."
 fi
 
 # Ping gateway
 print_info "Pinging default gateway..."
 set -f
-octets=(${ip//./ })
+octets=("${ip//./ }")
 gateway="${octets[0]}.${octets[1]}.${octets[2]}.1"
-if (( $verbose == 0 ))
+if (( verbose == 0 ))
 then
-    ping -c 1 $gateway 2>&1 >/dev/null
+    quiet_ping "$gateway" "Issue with router."
 else
-    ping -c 1 $gateway
-fi
-
-if [ $? -ne 0 ]
-then
-    print_error "Issue with router."
-    exit
+    verbose_ping "$gateway" "Issue with router."
 fi
 
 # Ping Google DNS server
 print_info "Pinging Google DNS server..."
-if (( $verbose == 0 ))
+if (( verbose == 0 ))
 then
-    ping -c 1 8.8.8.8 2>&1 >/dev/null
+    quiet_ping "8.8.8.8" "Issue with ISP."
 else
-    ping -c 1 8.8.8.8
-fi
-
-if [ $? -ne 0 ]
-then
-    print_error "Issue with ISP."
-    exit
+    verbose_ping "8.8.8.8" "Issue with ISP."
 fi
 
 # Ping google.com
 print_info "Pinging Google's domain name..."
-if (( $verbose == 0 ))
+if (( verbose == 0 ))
 then
-    ping -c 1 "google.com" 2>&1 >/dev/null
+    quiet_ping "google.com" "Issue with DNS." 
 else
-    ping -c 1 "google.com"
-fi
-
-if [ $? -ne 0 ]
-then
-    print_error "Issue with DNS."
-    exit
+    verbose_ping "google.com" "Issue with DNS."
 fi
 
 print_info "All good to go!"
