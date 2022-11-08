d9ab42c6bcbcbd009050d5885ea4c3ebae9d3ea0
aqc1 <aqc1@pct.edu>
2022-11-08 12:35:02 -0500

    feat(Python): Added ARP broadcast script with Scapy

---
+++ Python/arp_broadcast.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from scapy.all import *
+
+subnet = "10.0.0.157/24"
+
+def broadcast(ip: str) -> list:
+    """
+    Use an ARP broadcast for network discovery
+    :param subnet:
+        subnet for ARP packet
+    :return:
+        list of clients
+    """
+    # Create Packet
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = broadcast/arp_request
+    answered = srp(packet, timeout=1, verbose=False)[0]
+
+    # Gather clients from broadcast
+    clients = list()
+    for element in answered:
+        client = {
+            "ip": element[1].psrc,
+            "mac": element[1].hwsrc
+        }
+        clients.append(client)
+    return clients
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Network Discovery via ARP Broadcast"
+    )
+    parser.add_argument(
+        "--subnet",
+        "-s",
+        type=str,
+        help="Subnet to broadcast ARP packet (CIDR)",
+        required=True
+    )
+    args = parser.parse_args()
+
+    clients = broadcast(args.subnet)
+    for client in clients:
+        print(f"IP: {client['ip']:<15} MAC: {client['mac']}")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 Not saying this is good code...but just a few items put together to achieve small tasks or ideas when I get bored.
 
-- `Python/encode_ip.py`: A script to encode an IPv4 address various ways.
+- Bash
+    - `test_network.sh`: A script to test basic network connection.
 
-- `Python/type_juggling.py`: A script to scan source code for type juggling vulns.
+- Python
+    - `arp_broadcast`: A script to broadcast an ARP packet for network discovery (Requires root)
+    - `dns_enum.py`: A script to enumerate DNS records of a domain.
+    - `encode_ip.py`: A script to encode an IPv4 address various ways.
+    - `type_juggling.py`: A script to scan source code for type juggling vulns.
 
-- `Bash/test_network.sh`: A script to test basic network connection.
-
-- `Python/dns_enum.py`: A script to enumerate DNS records of a domain.
-
-- `PowerShell/Networkmath.ps1`: A PowerShell script to do some quick subnetting math.
+- PowerShell
+    - `Networkmath.ps1`: A PowerShell script to do some quick subnetting math.
